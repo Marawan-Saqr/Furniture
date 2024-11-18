0568d93fc064fb3aac2527a5b5d1ce0d9a0bfafd
Marawan Saqr <82732583+Marawan-Saqr@users.noreply.github.com>
2024-11-18 16:06:06 +0200

    Update ProductDetails.jsx

--- src/Pages/Main/Shop/Product-details/ProductDetails.jsx
+++ src/Pages/Main/Shop/Product-details/ProductDetails.jsx
@@ -12,7 +12,7 @@ const ProductDetails = () => {
 
   const getProductDetails = async () => {
     try {
-      const response = await axios.get(`http://localhost:3001/chairsProducts/${params.productID}`);
+      const response = await axios.get(`https://veil-flicker-piano.glitch.me/team/${params.productID}`);
       setProductDetails(response.data);
     } catch (error) {
       console.error("Error fetching product details:", error);
